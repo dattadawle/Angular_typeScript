864fcf0539722a33d3931f68b7884d238fc49e1e
Dawle Datta Lingram <dattadawle71@gmail.com>
2025-03-11 18:50:42 +0530

    angularcomponenet

---
+++ FirstAngularApp/src/app/second/second.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+@Component({
+    // selector:'app-second',
+   // selector: '[app-second]',
+    selector : '.app-second',
+   // template: "<h2> second component page</h2>"
+
+   templateUrl : './second.component.html', 
+   styles: [`
+    h1{
+     color: blue;
+    }
+    `]
+})
+export class SecondComponent{
+
+}
